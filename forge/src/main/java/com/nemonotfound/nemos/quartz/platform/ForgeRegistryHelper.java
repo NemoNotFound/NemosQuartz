2685cb8eb992ab2394868f6a2d33bb436a5e7d1a
NemoNotFound <unconfigured@null.spigotmc.org>
2026-02-08 13:37:56 +0100

    Update 1.21.11

--- forge/src/main/java/com/nemonotfound/nemos/quartz/platform/ForgeRegistryHelper.java
+++ forge/src/main/java/com/nemonotfound/nemos/quartz/platform/ForgeRegistryHelper.java
@@ -8,7 +8,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -47,7 +47,7 @@ public class ForgeRegistryHelper implements IRegistryHelper {
 
     @Override
     public <T extends Entity> Supplier<EntityType<T>> registerEntity(String id, EntityType.Builder<T> entityTypeBuilder) {
-        var resourceLocation = ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, id);
+        var resourceLocation = Identifier.fromNamespaceAndPath(Constants.MOD_ID, id);
         var resourceKey = ResourceKey.create(Registries.ENTITY_TYPE, resourceLocation);
 
         return ENTITIES.register(id, () -> entityTypeBuilder.build(resourceKey));
@@ -81,6 +81,6 @@ public class ForgeRegistryHelper implements IRegistryHelper {
     }
 
     private static <T> ResourceKey<T> createResourceKey(ResourceKey<Registry<T>> registryResourceKey, String id) {
-        return ResourceKey.create(registryResourceKey, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, id));
+        return ResourceKey.create(registryResourceKey, Identifier.fromNamespaceAndPath(Constants.MOD_ID, id));
     }
 }
